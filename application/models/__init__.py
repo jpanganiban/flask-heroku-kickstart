c38c64f00b7586ead4904a1f845fb6349f8bf6e3
Jesse Panganiban <me@jpanganiban.com>
2013-03-28 20:15:38 +0800

    Add User model

---
+++ application/models/__init__.py
@@ -0,0 +1,24 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+import hashlib
+
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True)
+    username = db.Column(db.String(64), unique=True)
+    password = db.Column(db.String(128), unique=True)
+
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+
+    def __init__(self, *args, **kwargs):
+        super(User, self).__init__(*args, **kwargs)
+        self.password = self._hash_password(kwargs["password"])
+
+    def _hash_password(self, password_string):
+        """Creates a sha1 hash equivalent of the password"""
+        # TODO: This function can be imporved further...
+        return hashlib.new('sha1', password_string).hexdigest()
