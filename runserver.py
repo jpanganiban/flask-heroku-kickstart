c70877b0d3f96f1cc83452d52d287459188ed0e6
Jesse Panganiban <me@jpanganiban.com>
2013-03-28 19:35:50 +0800

    Attempt to use gunicorn on written application

--- runserver.py
+++ runserver.py
@@ -1,24 +1,4 @@
-#from application import create_app
-#import os
-#
-#
-#if __name__ == '__main__':
-#    port = int(os.environ.get('PORT', 5000))
-#    create_app().run(debug=True, port=port)
-
-from flask import Flask
-import os
+from application import create_app
 
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return "Hello World!"
-
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    #app = create_app()
-    app.run(port=port)
+app = create_app()
