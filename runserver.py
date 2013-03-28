7e945cd8c1945566d226191c3f728b5ec1407cb9
Jesse Panganiban <me@jpanganiban.com>
2013-03-28 19:23:41 +0800

    A leaner test

--- runserver.py
+++ runserver.py
@@ -1,7 +1,24 @@
-from application import create_app
+#from application import create_app
+#import os
+#
+#
+#if __name__ == '__main__':
+#    port = int(os.environ.get('PORT', 5000))
+#    create_app().run(debug=True, port=port)
+
+from flask import Flask
 import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello World!"
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    create_app().run(debug=True, port=port)
+    #app = create_app()
+    app.run(debug=True, port=port)
