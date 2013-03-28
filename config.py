cb9640b40bf1029672f1ec8b38d7c817745d16e2
Jesse Panganiban <me@jpanganiban.com>
2013-03-28 19:41:48 +0800

    Add application secret_key

---
+++ config.py
@@ -0,0 +1,2 @@
+class Config(object):
+    SECRET_KEY = '27ccc159468b4d9f944347ca634ca2a9'
--- runserver.py
+++ runserver.py
@@ -1,4 +1,5 @@
 from application import create_app
+from config import Config
 
 
-app = create_app()
+app = create_app(Config())
