0cdbb2d1033a164893f3e2b34dea2b3b898b0692
Jesse Panganiban <me@jpanganiban.com>
2013-03-28 21:12:21 +0800

    Database rebuild

--- runserver.py
+++ runserver.py
@@ -6,13 +6,22 @@ import config
 app = create_app(config.ProductionConfig())
 
 
+# XXX: A special development route to be used to
+# rebuild the database.
+@app.route('/db-rebuild')
+def db_rebuild():
+    db.drop_all()
+    db.create_all()
+    return "Database rebuilt!"
+
+
 if __name__ == '__main__':
     app = create_app(config.DevelopmentConfig())
 
     # XXX: A special development route to be used to
     # rebuild the database.
     @app.route('/db-rebuild')
-    def db_rebuild():
+    def dev_db_rebuild():
         db.drop_all()
         db.create_all()
         return "Database rebuilt!"
